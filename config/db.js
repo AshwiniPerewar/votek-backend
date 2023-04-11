802ec52629f80a6cedf3884d2b5ce229b81967b8
AshwiniPerewar@gmail.com <ashwiniperewar411@gmail.com>
2023-04-12 02:16:06 +0530

    backend

--- config/db.js
+++ config/db.js
@@ -4,7 +4,6 @@ const firebase = require('firebase');
 require("dotenv").config();
 
 mongoose.set("strictQuery", false);
-
 const Connection= mongoose.connect(process.env.MONGO_URL);
 
 const firebaseConfig = {
